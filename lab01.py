1d86b9cdefb548e2c4243f7caf25e07803777f13
Nathan Tang <nathan.ly.tang@gmail.com>
2022-01-08 20:44:30 -0700

    Complete lab 1

--- lab01.py
+++ lab01.py
@@ -1,8 +1,12 @@
 import requests
 
-# Print requests package version
+# virtualenv Pt4. Print requests package version
 print(requests.__version__)
 
-# Get the Google homepage
+# curl Pt5. Get the Google homepage
 req = requests.get('https://www.google.com')
 print(req.status_code)
+
+# curl Pt10. Modify your Python script so that it downloads itself from GitHub and prints out its own source code from GitHub
+req = requests.get('https://raw.githubusercontent.com/nathanlytang/CMPUT404-Lab01/master/lab01.py')
+print(req.text)
